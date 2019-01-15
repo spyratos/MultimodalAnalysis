a799e59bd23b055604a2e4c90e988a3ed944ae65
kostis_parask <kostis.parask@gmail.com>
2019-01-15 12:32:36 +0200

    classifier of single file now classifier of folder...

--- download_classify_file.py
+++ download_classify_file.py
@@ -4,33 +4,25 @@ Created on Fri Nov 30 09:57:19 2018
 
 @author: kostis
 """
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 09:57:19 2018
+
+@author: kostis
+"""
 def main():
     user_input = input("Enter ID of youtube video: ")
     url='https://www.youtube.com/watch?v='+user_input
     
-    options = {
-        'format':'bestaudio/best',
-        'extractaudio':True,
-        'audioformat':'mp3',
-        'outtmpl': r'''C:\Users\kostis\downloads_dl\toclassify\%(id)s.%(ext)s''',     #name the file the ID of the video
-        'noplaylist':True,
-        'nocheckcertificate':True,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-        }]
-    }
-    with youtube_dl.YoutubeDL(options) as ydl:
-                ydl.download([str(user_input)])
-     
    
+  
     
-    data_folder = PureWindowsPath('downloads_dl/toclassify/')
-    filename= user_input + str('.wav')
-    file_to_open = data_folder / filename
-    print(aT.fileClassification(file_to_open, "svm_interviews","svm"))
+    os.system('youtube-dl --extract-audio --audio-format mp3 -o "/home/kostis/Desktop/MultimodalAnalysis1/toclassify/%(id)s.%(ext)s" ' + url)
+     
+    os.system('python audioAnalysis.py classifyFolder -i /home/kostis/Desktop/MultimodalAnalysis1/toclassify/ --model svm --classifier /home/kostis/Desktop/MultimodalAnalysis1/svm_inter --details')
     
+
+  
 if __name__ == "__main__":
     import youtube_dl
     import pandas as pd
