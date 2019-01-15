2173320eb7084a136116c14d86044a0ac7022d89
kostis_parask <kostis.parask@gmail.com>
2019-01-15 12:27:31 +0200

    downloader.py simplified with terminal commands

--- downloader.py
+++ downloader.py
@@ -5,63 +5,26 @@ Created on Mon Nov 26 18:18:43 2018
 @author: kostis
 """
 
+import youtube_dl
+import pandas as pd
+import youtube_dl
+import shutil
+import os
+import sys
 
-def main():
-    
-    
-    
-    user_input = input("Enter name of file: ")
-    df=pd.read_csv(user_input)
-    
-    
-    ##download only audio
-    options = {
-        'format':'bestaudio/best',
-        'extractaudio':True,
-        'audioformat':'mp3',
-        'outtmpl': r'''C:\Users\kostis\downloads_dl\positive\%(title)s-%(id)s.%(ext)s''',     #name the file the ID of the video
-        'noplaylist':True,
-        'nocheckcertificate':True,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-        }]
-    }
-    with youtube_dl.YoutubeDL(options) as ydl:
-        for i in range(len(df)):
-            if df['Label'][i]=='agreement':
-                ydl.download([str(df['url'][i])])
-                
-                
-        options = {
-        'format':'bestaudio/best',
-        'extractaudio':True,
-        'audioformat':'mp3',
-        'outtmpl': r'''C:\Users\kostis\downloads_dl\negative\%(title)s-%(id)s.%(ext)s''',     #name the file the ID of the video
-        'noplaylist':True,
-        'nocheckcertificate':True,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-        }]
-    }
-    with youtube_dl.YoutubeDL(options) as ydl:
-        for i in range(len(df)):
-            if df['Label'][i]=='disagreement':
-                ydl.download([str(df['url'][i])])
-            
-         
-              
-   
-if __name__ == "__main__":
-    import youtube_dl
-    import pandas as pd
-    import youtube_dl
-    import shutil
-    import os
-    import sys
-    main()
-    
- 
+
+
+df=pd.read_csv('/home/kostis/Desktop/MultimodalAnalysis1/test.csv')
+
+
+for i in range(len(df)):
+	if df['Label'][i]=='agreement':
+		song=df['url'][i]
+		os.system('youtube-dl --extract-audio --audio-format mp3 -o "/home/kostis/Desktop/MultimodalAnalysis1/positive/%(title)s.%(ext)s" ' + song)
+
+
+for i in range(len(df)):
+	if df['Label'][i]=='disagreement':
+		song=df['url'][i]
+		os.system('youtube-dl --extract-audio --audio-format mp3 -o "/home/kostis/Desktop/MultimodalAnalysis1/negative/%(title)s.%(ext)s" '+ song)
+	
